f204da49b638462c63df8ba240e2acb4d3b630ca
Nozima11 <nozimakhabibullayeva11@gmail.com>
2025-03-05 12:31:20 +0500

    Update strings.py

--- lesson-1/homework/strings.py
+++ lesson-1/homework/strings.py
@@ -1,1 +1,31 @@
+# 1
+a = input('a side: ')
+perimeter = (int(a) * 4)
+area = int(a) ** 2
+print(perimeter)
+print(area)
 
+# 2
+radius = input('r: ')
+area = int(radius)**2 * 3.14
+length = int(radius) * 2 * 3.14
+print(area)
+print(length)
+
+# 3
+a = input('a: ')
+b = input('b: ')
+mean = (int(a) + int(b)) // 2
+print(mean)
+
+# 4 
+a = input('a: ')
+b = input('b: ')
+sum = int(a) + int(b)
+product = int(a) * int(b)
+square_a = int(a) ** 2
+square_b = int(b) ** 2
+print(sum)
+print(product)
+print(square_a)
+print(square_b)
