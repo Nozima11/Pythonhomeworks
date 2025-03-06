219939dba8289b2ea8436987b9599d617fae1e2b
Nozima11 <nozimakhabibullayeva11@gmail.com>
2025-03-06 15:10:04 +0500

    Update homework4.py

--- lesson-4/homework/homework4.py
+++ lesson-4/homework/homework4.py
@@ -1,1 +1,36 @@
+# 1
+d = {'David': 11, 'Jack': 7, 'John': 2}
+asc = sorted(d.items(), key = lambda x:x[1])
+desc = sorted(d.items(), key=lambda x: x[1], reverse=True)
+print(asc)
+print(desc)
 
+# 2
+d = {'David': 11, 'Jack': 7, 'John': 2}
+d['Harry']=6
+print(d)
+
+# 3
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+dic1.update(dic2)
+dic1.update(dic3)
+print(dic1)
+
+# 4 
+n = 5
+d = {x: x*x for x in range (1, n+1)}
+print(d)
+
+# 5
+n = 15
+d = {x: x*x for x in range (1, n+1)}
+print(d)
+
+# Set exercises
+#1
+s = {1,2,3,4,5,6}
+print(s)
+
+# 2
